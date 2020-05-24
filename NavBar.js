4265c33e3648fb8e956ca9cd96a0507efca9a83c
Christos Andreou <charosgaming@hotmail.co.uk>
2020-05-24 23:50:26 +0300

    Added: 1) A placeholder alarm has been added for testing purposes.
    Changes:
    1) The Navigation bar & simple alarms have been moved to JavaScript instead of HTML. This change will allow the admin of the page to handle changes to the navigation bar and alarms much easier without having to deal with each page individually!

---
+++ NavBar.js
@@ -0,0 +1,44 @@
+var html = 
+'<nav  id="navigation-placeholder" class="navbar navbar-default navbar-expand-xl navbar-light bg-light">\
+    <a class="navbar-brand"><img class="CablenetLogo" src="Resources/Other/CablenetLogo.png"></a>\
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">\
+            <span class="navbar-toggler-icon"></span>\
+        </button>\
+        <div class="collapse navbar-collapse"id="navbarNav">\
+            <ul class="navbar-nav nav-fill w-100">\
+                <li class="nav-item"><a class="nav-link" href="Index.html">Home <span class="sr-only">(current)</span></a></li>\
+                <li class="nav-item"><a class="nav-link" href="Modems.html">Modems</a></li>\
+                <li class="nav-item"><a class="nav-link" href="Retail.html">Retail</a></li>\
+                <li class="nav-item"><a class="nav-link" href="SmartPhones.html">SmartPhones</a></li>\
+                <li class="nav-item"><a class="nav-link" href="Templates.html">Templates</a></li>\
+                <li class="nav-item"><a class="nav-link" href="Business.html">Business</a></li>\
+            </ul>\
+        </div>\
+</nav>\
+\
+\
+<!-- -->\
+<div class="alert alert-success alert-dismissible fade show" role="alert">\
+	Help fix Agents Portal by sending your findings to <a href = "mailto: c.andreou@cablenetcy.net">c.andreou@cablenetcy.net</a>\
+	<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>\
+</div>\
+\
+\
+<div class="alert alert-danger" role="alert">Node P8-1 DOWN</div>\
+\
+\
+';
+
+document.getElementById('NavigationBarReference').innerHTML = html;
+
+// Simple Notification for the nodes being down. The user's unable to disable these:
+
+// <div class="alert alert-danger" role="alert">Node P8-1 DOWN</div>\                         
+
+// Simple Notication for any other updates. The user's able to close these.
+{/* 
+    <div class="alert alert-success alert-dismissible fade show" role="alert">\
+	    Help fix Agents Portal by sending your findings to <a href = "mailto: c.andreou@cablenetcy.net">c.andreou@cablenetcy.net</a>\
+	    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>\
+    </div>\ 
+*/}
