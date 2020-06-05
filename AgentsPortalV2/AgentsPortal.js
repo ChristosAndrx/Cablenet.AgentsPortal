81fc9b076dc71bae8111ae34aaee8aee36e0d1ad
Christos Andreou <charosgaming@hotmail.co.uk>
2020-06-05 11:34:49 +0300

    Contributed to the code.

--- AgentsPortalV2/AgentsPortal.js
+++ AgentsPortalV2/AgentsPortal.js
@@ -6,18 +6,16 @@ html = '\
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">\
             <span class="navbar-toggler-icon"></span>\
         </button>\
-        <div class="collapse navbar-collapse"id="navbarNav">\
-            <ul class="navbar-nav nav-fill w-100">\
-                <li class="nav-item"><a class="nav-link disabled" aria-disabled href="Index.html">Home <span class="sr-only">(current)</span></a></li>\
-                <li class="nav-item"><a class="nav-link" href="Retail.html">Retail</a></li>\
-                <li class="nav-item"><a class="nav-link disabled" href="Business.html">Business</a></li>\
-                <li class="nav-item"><a class="nav-link" href="Templates.html">Templates</a></li>\
-                <li class="nav-item"><a class="nav-link" href="AgentTools.html">Agent Tools</a></li>\
+        <div class="collapse navbar-collapse" id="navbarNav">\
+            <ul class="navbar-nav nav-fill">\
+                <li class="nav-item"><a class="nav-link" href="Index.html"> Home </a></li>\
+                <li class="nav-item"><a class="nav-link" href="Retail.html"> Retail </a></li>\
+                <li class="nav-item"><a class="nav-link disabled" href="Business.html"> Business (Coming Soon) </a></li>\
+                <li class="nav-item"><a class="nav-link" href="Templates.html"> Templates </a></li>\
+                <li class="nav-item"><a class="nav-link" href="AgentTools.html"> Agent Tools </a></li>\
             </ul>\
         </div>\
 </nav>\
-\
-<div class="alert alert-danger" role="alert">Node P8-1 DOWN</div>\
 \
 <div class="alert alert-success alert-dismissible fade show" role="alert">\
 	Help improve Agents Portal by submitting your findings & suggestions: <a href = "mailto: c.andreou@cablenetcy.net">c.andreou@cablenetcy.net</a>\
